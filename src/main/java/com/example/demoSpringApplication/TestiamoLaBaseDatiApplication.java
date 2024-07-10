c23d2f9d5c1eac31a5693792bed0711651bcbc44
Ilaria Faleschini <ilaria.faleschini@gmail.com>
2024-07-10 15:37:20 +0200

    first commit

---
+++ src/main/java/com/example/demoSpringApplication/TestiamoLaBaseDatiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoSpringApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestiamoLaBaseDatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestiamoLaBaseDatiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoSpringApplication/TestiamoLaBaseDatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestiamoLaBaseDatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
